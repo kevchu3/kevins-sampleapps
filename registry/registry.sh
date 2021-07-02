dc6be725009ef48fc81951805bb4c2fe13960ec3
Kevin Chung <kevin.chung@redhat.com>
2021-07-02 17:07:31 -0400

    Updates for TLS support on registry

--- registry/registry.sh
+++ registry/registry.sh
@@ -2,21 +2,30 @@
 
 # Reference link: https://helm.sh/docs/topics/registries/
 
-htpasswd -cB -b auth.htpasswd admin admin123
+htpasswd -cB -b /opt/registry/auth.htpasswd admin admin123
 
-# In /etc/containers/registries.conf, update the following to add our registry to the insecure registries:
-# [registries.insecure]
-# registries = ['10.0.0.1:5000']
+# Set up TLS for registry
+mkdir -p /opt/registry/certs
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout /opt/registry/certs/myregistry.key -x509 -days 365 -out /opt/registry/certs/myregistry.crt
+cp /opt/registry/certs/myregistry.crt /etc/pki/ca-trust/source/anchors/
+update-ca-trust
 
-cp /etc/containers/registries.conf /etc/containers/registries.conf.bkp
-sed -e '/\[registries.insecure\]/{ n; c\' -e 'registries = \["10.0.0.1:5000"\]' -e '}' /etc/containers/registries.conf.bkp > /etc/containers/registries.conf
-rm /etc/containers/registries.conf.bkp
+mkdir -p /opt/registry/data
+mkdir -p /opt/registry/auth
 
 # Then restart podman
 systemctl restart podman
 
-podman run -dp 5000:5000 --restart=always --name registry \
-  -v $(pwd)/auth.htpasswd:/etc/docker/registry/auth.htpasswd:z \
-  -e REGISTRY_AUTH="{htpasswd: {realm: localhost, path: /etc/docker/registry/auth.htpasswd}}" \
-  registry
-rm auth.htpasswd
+podman run --name myregistry \
+-p 5000:5000 \
+-v /opt/registry/data:/var/lib/registry:z \
+-v /opt/registry/auth:/auth:z \
+-e "REGISTRY_AUTH=htpasswd" \
+-e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+-e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+-v /opt/registry/certs:/certs:z \
+-e "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/myregistry.crt" \
+-e "REGISTRY_HTTP_TLS_KEY=/certs/myregistry.key" \
+-e REGISTRY_COMPATIBILITY_SCHEMA1_ENABLED=true \
+-d \
+docker.io/library/registry:latest
